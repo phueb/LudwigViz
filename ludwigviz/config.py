c67369689c30a28b034228ce3b011f894ff6a03c
phueb <info@philhuebner.com>
2020-01-22 12:27:23 -0600

    change back to 'research_data'

--- ludwigviz/config.py
+++ ludwigviz/config.py
@@ -7,7 +7,7 @@ from ludwigviz import dummy_data, mnt_point
 class RemoteDirs:
 
     if dummy_data is None:
-        research_data = Path(mnt_point) / 'alternate_research_data'
+        research_data = Path(mnt_point) / 'research_data'
         if not os.path.ismount(str(research_data)):
             print('WARNING: {} not mounted.'
                   'Using dummy directory for development'.format(research_data))
